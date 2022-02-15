4660ae4168a68920ad2f85418016cde370c27445
Shpakouskaya_Mariya <Shpackovskaya.maria@yandex.by>
2022-02-15 20:17:45 +0300

    Classes based on MyDinamicArray

--- university-Shpakouskaya/src/com/belhard/university/Department.java
+++ university-Shpakouskaya/src/com/belhard/university/Department.java
@@ -1,84 +1,76 @@
 package com.belhard.university;
 
-public class Department {
-	private String nameOfDepartment;
+import com.belhard.university.datastructures.MyDynamicArray;
+
+public class Department implements Identifiable {
+	private static long counter = 0;
+	private long id;
+	private NameOfDepartment name;
 	private Teacher headOfDepartment;
 	private Teacher deputyHeadOfDepartment;
 	private Cleaner cleaner;
-	private final Teacher[] teachers = new Teacher[8];
-	private int numberOfTeachers;
+	private final MyDynamicArray teachers = new MyDynamicArray();
+	private int maxNumberOfTeacher = 8;
+
+	public Department() {
+		id = ++counter;
+	}
+
+	@Override
+	public long getId() {
+		return id;
+	}
 
 	public boolean addTeacher(Teacher teacher) {
-		if (numberOfTeachers < teachers.length) {
-			teachers[numberOfTeachers++] = teacher;
+		if (teachers.size() < maxNumberOfTeacher) {
+			teachers.add(teacher);
 			return true;
 		}
 		return false;
 	}
 
-	public boolean dismissTeacher(Teacher teacher) {
-		boolean dismiss = false;
-		for (int i = 0; i < numberOfTeachers; i++) {
-			Teacher elm = teachers[i];
-			if (elm.getId() == teacher.getId()) {
-				teachers[i] = null;
-				dismiss = true;
-			}
-			if (dismiss == true) {
-				if (i != numberOfTeachers - 1) {
-					teachers[i] = teachers[i + 1];
-				} else {
-					teachers[i] = null;
-				}
-			}
-		}
-		if (dismiss) {
-			numberOfTeachers--;
-		}
-		return dismiss;
+	public boolean removeTeacher(Teacher teacher) {
+		return teachers.remove(teacher);
+	}
+
+	public boolean containsTeacher(Teacher teacher) {
+		return teachers.contains(teacher);
+	}
+
+	public String getTeacher(int index) {
+		Teacher teacher = (Teacher) teachers.get(index);
+		String list = "[id:" + teacher.getId() + "] " + teacher.getFirstName() + " " + teacher.getLastName() + "\n";
+		return list;
 	}
 
 	public String getList() {
-		String list = "<<<<<<<  Department of " + nameOfDepartment + "  >>>>>>>\n";
+		String list = "<<<<<<<  Department of " + getName() + "  >>>>>>>\n";
 		list += "Head of Department: ";
 		if (headOfDepartment != null) {
-			list += "[id: " + headOfDepartment.getId() + "] " + headOfDepartment.getFirstName() + " "
-					+ headOfDepartment.getLastName() + "\n";
+			list += headOfDepartment.toString();
 		} else {
 			list += "NOT APPOINTED\n";
 		}
 		list += "Deputy head of Department: ";
 		if (deputyHeadOfDepartment != null) {
-			list += "[id: " + deputyHeadOfDepartment.getId() + "] " + deputyHeadOfDepartment.getFirstName() + " "
-					+ deputyHeadOfDepartment.getLastName() + "\n";
+			list += deputyHeadOfDepartment.toString();
 		} else {
 			list += "NOT APPOINTED\n";
 		}
 		list += "Teachers of Department:\n";
-		for (int i = 0; i < teachers.length; i++) {
-			Teacher teacher = teachers[i];
-			if (teacher == null) {
-				break;
-			}
-			list += (i + 1) + ". [id:" + teacher.getId() + "] " + teacher.getFirstName() + " " + teacher.getLastName()
-					+ "\n";
+		Object[] array = teachers.toArray();
+		for (int i = 0; i < array.length; i++) {
+			Teacher teacher = (Teacher) array[i];
+			list += (i + 1) + ". " + teacher.toString();
 		}
+
 		list += "Cleaner: ";
 		if (cleaner != null) {
-			list += "[id: " + cleaner.getId() + "] " + cleaner.getFirstName() + " " + cleaner.getLastName() + " "
-					+ cleaner.getParlor() + "\n";
+			list += cleaner.toString() + ".\n";
 		} else {
 			list += "NOT APPOINTED\n";
 		}
 		return list;
-	}
-
-	public String getNameOfDepartment() {
-		return nameOfDepartment;
-	}
-
-	public void setNameOfDepartment(String nameOfDepartment) {
-		this.nameOfDepartment = nameOfDepartment;
 	}
 
 	public Teacher getHeadOfDepartment() {
@@ -105,18 +97,22 @@ public class Department {
 		this.cleaner = cleaner;
 	}
 
-	public int getNumberOfTeachers() {
-		return numberOfTeachers;
+	public MyDynamicArray getTeachers() {
+		return teachers;
 	}
 
-	public void setNumberOfTeachers(int numberOfTeachers) {
-		this.numberOfTeachers = numberOfTeachers;
+	public NameOfDepartment getName() {
+		return name;
+	}
+
+	public void setName(NameOfDepartment name) {
+		this.name = name;
 	}
 
 	@Override
 	public String toString() {
-		int numberOfFreePlaces = teachers.length - numberOfTeachers;
-		return getList() + "\tNumber of teachers: " + numberOfTeachers + "\n" + "\tFree places in the department: "
+		int numberOfFreePlaces = maxNumberOfTeacher - teachers.size();
+		return getList() + "\tNumber of teachers: " + teachers.size() + "\n" + "\tFree places in the department: "
 				+ numberOfFreePlaces + "\n";
 	}
 
